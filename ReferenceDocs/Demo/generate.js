efcd6604ccc696c1e8688ecd180a48b92502bc6b
tbnXC <bwc1024@hotmail.com>
2023-02-05 21:28:32 -0500

    made it easier to modify the chords

--- ReferenceDocs/Demo/generate.js
+++ ReferenceDocs/Demo/generate.js
@@ -1,3 +1,6 @@
+var input = "F7 Bb7 F7 F7 Bb7 Bdim7 F7 D7 Gmin7 C7 F7 C7";
+// var input = "F7 E7 D7 G7 A7 Ab7 Dmin7 Bb7";
+
 var abc =
 `X: 1
 T: Demo
@@ -10,24 +13,28 @@ Q: 120
 |"Bb7"z8|"Bdim7"z8|"F7"z8|"D7"z8|
 |"G-7"z8|"C7"z8|"F7"z8|"C7"z8|]`;
 
+var chords = input.split(" ").map(chord => 
+    Tonal.Chord.getChord(Tonal.Chord.get(chord).aliases[0], Tonal.Chord.get(chord).tonic + "4"));
 
-var chords = 
-[
-    Tonal.Chord.getChord(Tonal.Chord.get("F7").aliases[0], Tonal.Chord.get("F7").tonic + "4"),
-    Tonal.Chord.getChord(Tonal.Chord.get("Bb7").aliases[0], Tonal.Chord.get("Bb7").tonic + "4"),
-    Tonal.Chord.getChord(Tonal.Chord.get("F7").aliases[0], Tonal.Chord.get("F7").tonic + "4"),
-    Tonal.Chord.getChord(Tonal.Chord.get("F7").aliases[0], Tonal.Chord.get("F7").tonic + "4"),
+console.log(chords);
 
-    Tonal.Chord.getChord(Tonal.Chord.get("Bb7").aliases[0], Tonal.Chord.get("Bb7").tonic + "4"),
-    Tonal.Chord.getChord(Tonal.Chord.get("Bdim7").aliases[0], Tonal.Chord.get("Bdim7").tonic + "4"),
-    Tonal.Chord.getChord(Tonal.Chord.get("F7").aliases[0], Tonal.Chord.get("F7").tonic + "4"),
-    Tonal.Chord.getChord(Tonal.Chord.get("D7").aliases[0], Tonal.Chord.get("D7").tonic + "4"),
+// var chords = 
+// [
+//     Tonal.Chord.getChord(Tonal.Chord.get("F7").aliases[0], Tonal.Chord.get("F7").tonic + "4"),
+//     Tonal.Chord.getChord(Tonal.Chord.get("Bb7").aliases[0], Tonal.Chord.get("Bb7").tonic + "4"),
+//     Tonal.Chord.getChord(Tonal.Chord.get("F7").aliases[0], Tonal.Chord.get("F7").tonic + "4"),
+//     Tonal.Chord.getChord(Tonal.Chord.get("F7").aliases[0], Tonal.Chord.get("F7").tonic + "4"),
 
-    Tonal.Chord.getChord(Tonal.Chord.get("Gmin7").aliases[0], Tonal.Chord.get("Gmin7").tonic + "4"),
-    Tonal.Chord.getChord(Tonal.Chord.get("C7").aliases[0], Tonal.Chord.get("C7").tonic + "4"),
-    Tonal.Chord.getChord(Tonal.Chord.get("F7").aliases[0], Tonal.Chord.get("F7").tonic + "4"),
-    Tonal.Chord.getChord(Tonal.Chord.get("C7").aliases[0], Tonal.Chord.get("C7").tonic + "4"),
-];
+//     Tonal.Chord.getChord(Tonal.Chord.get("Bb7").aliases[0], Tonal.Chord.get("Bb7").tonic + "4"),
+//     Tonal.Chord.getChord(Tonal.Chord.get("Bdim7").aliases[0], Tonal.Chord.get("Bdim7").tonic + "4"),
+//     Tonal.Chord.getChord(Tonal.Chord.get("F7").aliases[0], Tonal.Chord.get("F7").tonic + "4"),
+//     Tonal.Chord.getChord(Tonal.Chord.get("D7").aliases[0], Tonal.Chord.get("D7").tonic + "4"),
+
+//     Tonal.Chord.getChord(Tonal.Chord.get("Gmin7").aliases[0], Tonal.Chord.get("Gmin7").tonic + "4"),
+//     Tonal.Chord.getChord(Tonal.Chord.get("C7").aliases[0], Tonal.Chord.get("C7").tonic + "4"),
+//     Tonal.Chord.getChord(Tonal.Chord.get("F7").aliases[0], Tonal.Chord.get("F7").tonic + "4"),
+//     Tonal.Chord.getChord(Tonal.Chord.get("C7").aliases[0], Tonal.Chord.get("C7").tonic + "4"),
+// ];
 
 notes = 
 `X: 1
@@ -60,8 +67,8 @@ for (const chord of chords) {
     }
 }
 
-console.log(chords);
-for (let i of notes.split("\n")) console.log(i);
+// console.log(chords);
+// for (let i of notes.split("\n")) console.log(i);
 
 function load() {
     ABCJS.renderAbc("paper", notes, {jazzchords: true, initialClef: true, initialKey: true, add_classes:true, hideKeySignature: true});
